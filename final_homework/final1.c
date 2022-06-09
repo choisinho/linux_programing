2b097e0915089e4d25026cf19b063d7eb1608eb1
Sinho <choi_sinho@naver.com>
2022-06-09 14:00:23 +0900

    final1 finished

---
+++ final_homework/final1.c
@@ -0,0 +1,32 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/type.h>
+
+main()
+{
+    pid_t pid, ret;
+    int status = 0;
+    char in_buf[256];
+
+    while (1)
+    {
+        printf("$");
+        fgets(in_buf, 256, stdin);
+        in_buf[strlen(in_buf) - 1] = '\0';
+        
+        if (pid = fork() > 0)
+        {
+            ret = waitpid(pid, &status, 0);
+            printf("%s\n", WIFEXITED(status) ? "성공" : "실패");
+        }
+        else if (pid == 0)
+        {
+            execlp(in_buf, in_buf, NULL);
+        }
+        else
+        {
+            printf("fail to fork\n");
+        }
+    }
+    return 0;
+}
